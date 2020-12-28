9021f369ae1a701f3f02a4b6dd42ea3dc03290bc
Denis <krafft90@gmail.com>
2020-12-28 14:57:58 +0300

    New font and fixes

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,43 @@
 <template>
-  <div id="app" class="mh-100">
-    <div class="container d-flex flex-column pt-10 pb-6 mh-100">
+  <div id="app" class="min-vh-100">
+    <div class="container d-flex flex-column pt-10 pb-6 min-vh-100">
       <div class="row">
         <div class="col-xl-6 col-lg-8 col-md-10 mx-auto">
           <h1 class="h2">Сколько весит конверт</h1>
-          <p class="text-secondary small">Если на конверт нанесена литера А (такие конверты обычно называют маркированные), то
+          <p class="text-secondary small">Если на конверт нанесена литера А (такие конверты обычно называют
+            маркированные), то
             в его стоимость уже входит вес 20 гр.</p>
-          <b-form-group label="Маркировка на конверте">
+          <b-form-group label="Маркировка на конверте" class="mb-6">
             <b-form-radio v-model="isMarked" name="marked" :value="true">Маркированный</b-form-radio>
             <b-form-radio v-model="isMarked" name="marked" :value="false">Не маркированный</b-form-radio>
           </b-form-group>
-          <b-form-group label="Тип конверта">
+          <b-form-group label="Тип конверта" class="mb-6">
             <div class="d-flex flex-wrap">
               <label class="envelope-radio h-100" v-for="item in envelopsOptions" :key="item.value">
-                <input type="radio" name="envelope" v-model="envelopWeight" class="envelope-radio__input" :value="item.value"
+                <input type="radio" name="envelope" v-model="envelopWeight" class="envelope-radio__input"
+                       :value="item.value"
                        :checked="item.value === envelopWeight" @change="calc">
                 <span class="envelope-radio__label">
                   <img :src="item.src" class="envelope-radio__img" :alt="'Конверт ' + item.text">
-                  <h6 class="m-0">{{item.text}}</h6>
+                  <p class="font-weight-bolder m-0">{{item.text}}</p>
                   <span class="envelope-radio__help" v-b-tooltip.hover :title="item.desc">?</span>
                 </span>
               </label>
             </div>
           </b-form-group>
           <b-form-group label="Количество листов А4">
-            <b-form-input v-model="sheetsCount" type="number" placeholder="Количество листов" required @change="calc" @focus="$event.target.select()"/>
+            <b-form-input v-model="sheetsCount" type="number" placeholder="Количество листов" required @change="calc"
+                          @focus="$event.target.select()"/>
           </b-form-group>
           <div class="mt-10">
             <div class="alert alert-info mb-4" v-if="!isMarked">Необходимо доклеить марку с литерой А</div>
-            <h6><span class="text-secondary">Вес конверта с листами:</span> {{totalWeight}} гр.</h6>
-            <h6><span class="text-secondary">Номинал марок:</span> {{totalStamps ? totalStamps + ' коп.' :
-              'дополнительные марки не нужны'}}</h6>
+            <p class="font-weight-medium m-0">
+              <span class="text-secondary">Вес конверта с листами:</span> <span class="h5 font-weight-bold">{{totalWeight}} гр.</span>
+            </p>
+            <p class="font-weight-medium">
+              <span class="text-secondary">Номинал марок:</span> <span class="h5 font-weight-bold">{{totalStamps ? totalStamps + ' коп.' :
+              'дополнительные марки не нужны'}}</span>
+            </p>
           </div>
           <div class="mt-10 p-4 bg-accent-50 rounded">
             <b-link v-b-toggle="'collapse'" class="mb-4">Как производится рассчет?</b-link>
@@ -84,6 +91,9 @@
         stepPrice: 4
       };
     },
+    mounted() {
+      this.calc();
+    },
     methods: {
       calc() {
         this.totalWeight = this.sheetsCount * this.sheetWeight + this.envelopWeight;
@@ -92,6 +102,8 @@
           const steps = Math.ceil((this.totalWeight - 20) / this.stepWeight);
 
           this.totalStamps = steps * this.stepPrice;
+        } else {
+          this.totalStamps = 0;
         }
       }
     }
